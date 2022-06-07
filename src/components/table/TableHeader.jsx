88adb168bc0910edeafc80572dbdceaa33bba5ae
Vlad Mitroshin <vlad.mitroshin.99@gmail.com>
2022-06-08 01:50:00 +0300

    Refactor styles - table styles; - modal styles; - app styles.

--- src/components/table/TableHeader.jsx
+++ src/components/table/TableHeader.jsx
@@ -3,14 +3,26 @@ import './TableHeader.scss';
 
 const TableHeader = () => {
   return (
-    <thead>
-      <tr>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name</th>
-        <th scope="col">Phone</th>
-        <th scope="col">Email</th>
-        <th scope="col">&nbsp;</th>
-        <th scope="col">&nbsp;</th>
+    <thead className="table__thead thead">
+      <tr className="thead__tr">
+        <th className="thead__th" scope="col">
+          First Name
+        </th>
+        <th className="thead__th" scope="col">
+          Last Name
+        </th>
+        <th className="thead__th" scope="col">
+          Phone
+        </th>
+        <th className="thead__th" scope="col">
+          Email
+        </th>
+        <th className="thead__th" scope="col">
+          &nbsp;
+        </th>
+        <th className="thead__th" scope="col">
+          &nbsp;
+        </th>
       </tr>
     </thead>
   );
--- src/components/table/TableRow.jsx
+++ src/components/table/TableRow.jsx
@@ -5,23 +5,31 @@ import { FaEdit, FaTimes } from 'react-icons/fa';
 
 const TableRow = ({ record, openEditModal, deleteRecord }) => {
   return (
-    <tr scope="row" className="table__row">
-      <td data-label="First Name">{record.firstName}</td>
-      <td data-label="Second Name">{record.lastName}</td>
-      <td data-label="Phone">{record.phone}</td>
-      <td data-label="Email">{record.email}</td>
-      <td data-label="Edit">
-        <span className="table__btn-wrapper">
+    <tr scope="row" className="tbody__tr">
+      <td className="tbody__td" data-label="First Name">
+        {record.firstName}
+      </td>
+      <td className="tbody__td" data-label="Second Name">
+        {record.lastName}
+      </td>
+      <td className="tbody__td" data-label="Phone">
+        {record.phone}
+      </td>
+      <td className="tbody__td" data-label="Email">
+        {record.email}
+      </td>
+      <td className="tbody__td" data-label="Edit">
+        <span className="tbody__btn-wrapper">
           <FaEdit
-            className="table__edit-btn"
+            className="tbody__btn-edit"
             onClick={() => openEditModal(record)}
           />
         </span>
       </td>
-      <td data-label="Delete">
-        <span className="table__btn-wrapper">
+      <td className="tbody__td" data-label="Delete">
+        <span className="tbody__btn-wrapper">
           <FaTimes
-            className="table__delete-btn"
+            className="tbody__btn-delete"
             onClick={() => deleteRecord(record.id)}
           />
         </span>
